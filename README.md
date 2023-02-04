d80295dc9068e3947652f6ad023a67cf846a8f81
tomascardososerra <tomascardososerra@gmail.com>
2023-02-04 23:58:39 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/robot.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/> Fallback Chatgpt
+This is a mycroft fallback skill to query chatgpt
+
+## About
+It will address the fallback question to chatgpt
+
+## Examples
+* "Fallback chatgpt"
+
+## Credits
+Tomás Cardoso Serra
+
+## Category
+**Productivity**
+Information
+
+## Tags
+#Chatgpt
+#Fallback mycroft
+
---
+++ __init__.py
@@ -0,0 +1,43 @@
+from mycroft import FallbackSkill, intent_handler
+import requests
+import urllib
+import json
+
+api_endpoint = "https://api.openai.com/v1/completions"
+api_key = [YOUR KEY HERE]
+model = "text-davinci-001"
+
+# Define the request headers
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + api_key
+}
+
+class FallbackChatgpt(FallbackSkill):
+    def __init__(self):
+        FallbackSkill.__init__(self)
+
+    def initialize(self):
+        self.register_fallback(self.handle_fallback_ChatGPT, 8)
+
+    def handle_fallback_ChatGPT(self, message):
+        try:
+            payload = {
+                "model": model,
+                "prompt": message.data['utterance'],
+                "temperature": 0.4,
+                "top_p": 1,
+                "frequency_penalty": 0,
+                "presence_penalty": 0
+            }
+            response = requests.post(api_endpoint, headers=headers, data=json.dumps(payload), timeout=100)
+            response_json = response.json()
+            response = response_json["choices"][0]["text"]
+            self.speak(response)
+            return True
+        except:
+            self.log.info("error in ChatGPT fallback request")
+            return False
+
+def create_skill():
+    return FallbackChatgpt()
